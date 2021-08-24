e824ca8cd2e23ee6c78057aa8e55e0ab5e7fee89
Shannon <shannonjburris@gmail.com>
2021-08-23 20:12:30 -0400

    file structure

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+
+    return (
+<footer>
+<container>
+
+</container>
+</footer>
+
+)
+};
+
+export default Footer;
---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1 id="about-me">About Page</h1>
+      <container>
+        <section id="about-me-image">
+          <img src="/public/Assets/Images/IMG_3676.jpg" alt="me" />
+          </section>
+        </container>
+        
+    </div>
+
+  );
+}
