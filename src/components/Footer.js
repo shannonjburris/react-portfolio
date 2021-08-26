9e5ab807ea7c0fec72e4f609b5810126162d0ae7
Shannon <shannonjburris@gmail.com>
2021-08-25 23:00:06 -0400

    reordered scripts

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,15 +6,15 @@ function Footer() {
         <footer>
             <div class="container">
                 <div class="links">
-                <a href="https://github.com/shannonjburris" target="_blank"><i class="fab fa-github"></i></a>
+                <a href="https://github.com/shannonjburris" target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a>
                 </div>
             </div>
             <div class="container">
                 <ul class="contact-links">
                     <li> <a href="tel:863-255-6043">863-255-6043</a> </li>
-                    <li> <a href="mailto:shannonjburris@gmail.com" target="_blank">shannonjburris@gmail.com</a> </li>
-                    <li> <a href="https://github.com/shannonjburris" target="_blank">GitHub</a> </li>
-                    <li> <a href="https://www.linkedin.com/in/shannon-burris/" target="_blank">Linkedin</a></li>
+                    <li> <a href="mailto:shannonjburris@gmail.com" target="_blank" rel="noreferrer">shannonjburris@gmail.com</a> </li>
+                    <li> <a href="https://github.com/shannonjburris" target="_blank" rel="noreferrer">GitHub</a> </li>
+                    <li> <a href="https://www.linkedin.com/in/shannon-burris/" target="_blank" rel="noreferrer">Linkedin</a></li>
                 </ul>
             </div>
         </footer>
